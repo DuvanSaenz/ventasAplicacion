85d65cf0d108be2a86c2441e03d91fc3d393c8e5
Duvan <saenzherreraduvan@gmail.com>
2022-09-04 18:39:07 -0500

    Se realizo el diselo de la aplicacion

--- ventas/src/App.js
+++ ventas/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";  
+import "@fortawesome/fontawesome-free/js/all";
+import "bulma/css/bulma.css";
+import Layout from "./components/Layout";
 
 
 function App() {
   return (
     <div>
-      
+      <Layout/>
     </div>
 
   );
---
+++ ventas/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return(
+
+        <div className = "navbar is-primary">
+            <div className="navbar-bram">
+                <a href="/" className="navbar-item">
+                    <img src ="logo.png" alt ="logo"/>
+                </a>
+            </div>
+        </div>
+    
+
+    );
+}
+export default Header;
